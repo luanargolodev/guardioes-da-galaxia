48ce7fedc4bc3dc44d935a5464eddcce711015b3
Luan Lemos <ipsluan@icloud.com>
2023-06-16 22:33:46 -0300

    feat: add carousel

--- script/index.js
+++ script/index.js
@@ -6,3 +6,19 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0
+const images = document.getElementById('images')
+
+function navigationMember(direction) {
+  activeMember += direction
+  const member = members[activeMember]
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)`
+}
+
+const btnPrev = document.getElementById('btn-prev')
+const btnNext = document.getElementById('btn-next')
+
+btnPrev.addEventListener('click', () => navigationMember(-1))
+btnNext.addEventListener('click', () => navigationMember(1))
